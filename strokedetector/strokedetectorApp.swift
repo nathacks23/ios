9d73d1565f3d5ede496c53a64a1f08e724aa20c5
n33lp <canada0706@gmail.com>
2023-11-19 00:34:07 -0700

    Initial Commit

---
+++ strokedetector/strokedetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  strokedetectorApp.swift
+//  strokedetector
+//
+//  Created by Neel Patel on 2023-11-19.
+//
+
+import SwiftUI
+
+@main
+struct strokedetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
