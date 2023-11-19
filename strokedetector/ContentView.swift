d08dcdb94f76bfcc40bcdd07e221fb11b83755c7
n33lp <canada0706@gmail.com>
2023-11-19 04:38:31 -0700

    works

--- strokedetector/ContentView.swift
+++ strokedetector/ContentView.swift
@@ -1,26 +1,143 @@
-//
-//  ContentView.swift
-//  strokedetector
-//
-//  Created by Neel Patel on 2023-11-19.
-//
-
 import SwiftUI
+import CoreLocation
 
 struct ContentView: View {
+    @State private var isImagePickerPresented = false
+    @State private var selectedImage: UIImage?
+    @State private var apiResponse: String?
+    @State private var isShowingResults = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                if let image = selectedImage {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                        .padding()
+
+                    Button("Send to API") {
+                        if let imageData = image.jpegData(compressionQuality: 0.9) {
+                            sendImageToAPI(imageData: imageData)
+                            // Set the flag to show results view
+                            isShowingResults = true
+                        }
+                    }
+                    .padding()
+
+                    if let response = apiResponse {
+                        Text("API Response: \(response)")
+                            .padding()
+                    }
+                } else {
+                    Text("No selfie taken")
+                        .padding()
+                }
+
+                Button("Take Selfie") {
+                    isImagePickerPresented.toggle()
+                }
+                .padding()
+                .sheet(isPresented: $isImagePickerPresented) {
+                    ImagePicker(selectedImage: $selectedImage)
+                }
+            }
+            .navigationTitle("Stroke Detector App")
+            .background(
+                NavigationLink(
+                    destination: ResultsView(apiResponse: $apiResponse),
+                    isActive: $isShowingResults,
+                    label: { EmptyView() }
+                )
+            )
         }
-        .padding()
+    }
+
+    func sendImageToAPI(imageData: Data) {
+        // Your existing API request logic
+
+        // For example, updating the response
+        apiResponse = "API Response Placeholder"
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ResultsView: View {
+    @Binding var apiResponse: String?
+    @State private var location: CLLocationCoordinate2D?
+
+    var body: some View {
+        VStack {
+            Text("Results")
+                .font(.title)
+                .padding()
+
+            if let response = apiResponse, response != "1" {
+
+                VStack {
+                    Button(action: {
+                        makeEmergencyCall()
+                    }) {
+                        Text("Call 911")
+                            .foregroundColor(.red)
+                            .padding()
+                    }
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.red, lineWidth: 2)
+                    )
+                    .padding()
+                    
+                    if let currentLocation = location {
+                        Text("Current Location: \(currentLocation.latitude), \(currentLocation.longitude)")
+                            .padding()
+                    } else {
+                        Text("Location not available")
+                            .padding()
+                    }
+                }
+            } else {
+                Text("Don't Call 911")
+                    .foregroundColor(.red)
+                    .padding()
+
+                if let currentLocation = location {
+                    Text("Current Location: \(currentLocation.latitude), \(currentLocation.longitude)")
+                        .padding()
+                } else {
+                    Text("Location not available")
+                        .padding()
+                }
+            }
+        }
+        .onAppear {
+            getLocation()
+        }
+        .navigationTitle("Results")
+    }
+    
+    // Function to initiate the emergency call
+    func makeEmergencyCall() {
+        guard let phoneURL = URL(string: "tel://5879202662"), UIApplication.shared.canOpenURL(phoneURL) else {
+            return
+        }
+        UIApplication.shared.open(phoneURL)
+    }
+    
+    func getLocation() {
+        // Use Core Location to get the current location
+        // Make sure to handle location permissions in your app
+        // Here, I'm using a simple CLLocationManager setup for demonstration purposes
+
+        let locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+
+            if let location = locationManager.location?.coordinate {
+                self.location = location
+            }
+        }
     }
 }
---
+++ strokedetector/ImagePicker.swift
@@ -0,0 +1,47 @@
+//
+//  ImagePicker.swift
+//  strokedetector
+//
+//  Created by Neel Patel on 2023-11-19.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var selectedImage: UIImage?
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        @Binding var selectedImage: UIImage?
+
+        init(selectedImage: Binding<UIImage?>) {
+            _selectedImage = selectedImage
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                selectedImage = image
+            }
+
+            picker.dismiss(animated: true, completion: nil)
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            picker.dismiss(animated: true, completion: nil)
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(selectedImage: $selectedImage)
+    }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = .camera
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {}
+}
